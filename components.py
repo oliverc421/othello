6fb87bccef8879e7f663d06a0b20c37e2de62fa9
Oliver Chaisty <oliverchaisty@gmail.com>
2025-11-25 17:41:24 +0000

    legal move 2

--- components.py
+++ components.py
@@ -12,7 +12,7 @@ def initialise_board(size=8):
 def print_board():
 	print(initialise_board())	
 
-def legal_move(colour: str, coordinate: tuple, board: list) -> bool
+def legal_move(colour: str, coordinate: tuple, board: list) -> bool:
 	if board[coordinate[0]][coordinate[1]] != 'None ':
 		return False	
 	if colour == 'Light ':
@@ -24,12 +24,15 @@ def legal_move(colour: str, coordinate: tuple, board: list) -> bool
 		for j in range (-1,2):
 			try:
 				if board[(coordinate[0]+i)][(coordinate[1]+j)] == opponent:	# i want to capture this direction to check if there is a same colour counter further down
-					for k in range (2,8):
+					for k in range (1,8):
 						try:
-							if board[(coordinate[0]+k*i)][(coordinate[1]+k*j)] == colour: # timesing k by i and j means it goes in the 														specfic direction it needs to, if the target counter is 													to the left (coordinate(0) - 1) then continuing to minus 														is what i want to do
-								return True
+							if board[(coordinate[0]+k*i)][(coordinate[1]+k*j)] != opponent: # timesing k by i and j means it goes in the 														specfic direction it needs to, if the target counter is 													to the left (coordinate(0) - 1) then continuing to minus 														is what i want to do
+								if board[(coordinate[0]+k*i)][(coordinate[1]+k*j)] == 'None ':
+									break
+								else:
+									return True
 						except: 
 							break
-					except:
-						pass
+			except:
+				pass
 			
