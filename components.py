7b13a0771c0a1af2d3bab79e59f8c17741eed6da
Oliver Chaisty <oliverchaisty@gmail.com>
2025-12-01 18:16:05 +0000

    Working python implementation of Othello

--- components.py
+++ components.py
@@ -9,13 +9,13 @@ def initialise_board(size=8):
 		 ['None ', 'None ', 'None ', 'None ', 'None ', 'None ', 'None ', 'None ']]
 	return board
 
-def print_board():
-	print(initialise_board())	
+def print_board(board):
+	for i in board:
+		print(i)
 
 def legal_move(colour: str, coordinate: tuple, board: list) -> bool:
 	x, y = coordinate
-	if board[x][y] != 'None ':
-		print("There is already a counter on this square. Select different coordinates")
+	if board[y][x] != 'None ':
 		return False	
 	if colour == 'Light':
 		opponent = 'Dark '
@@ -25,14 +25,14 @@ def legal_move(colour: str, coordinate: tuple, board: list) -> bool:
 	for i in range (-1, 2):			# checking all the squares around the selected square
 		for j in range (-1,2):
 			try:
-				if board[(x+i)][(y+j)] == opponent:	# i want to capture this direction to check if there is 														a same colour counter further down
+				if board[(y+i)][(x+j)] == opponent:	# i want to capture this direction to check if there is 														a same colour counter further down
 					for k in range (1,8):
 						try:
-							if board[(x+k*i)][(y+k*j)] != opponent: # timesing k by i and j means it goes in the 
+							if board[(y+k*i)][(x+k*j)] != opponent: # timesing k by i and j means it goes in the 
 												# specfic direction it needs to, if the target
 												# counter is to the left (coordinate(0) - 1) then 
 												# continuing to minus is what i want to do
-								if board[(x+k*i)][(y+k*j)] == 'None ':	# if we hit a blank square, exit the nested loop and check the other squares
+								if board[(y+k*i)][(x+k*j)] == 'None ':	# if we hit a blank square, exit the nested loop and check the other squares
 									break
 								else:
 									return True
@@ -40,4 +40,3 @@ def legal_move(colour: str, coordinate: tuple, board: list) -> bool:
 							break
 			except:
 				pass
-	print("This is not a legal move. Select different coordinates.")			
--- game_engine.py
+++ game_engine.py
@@ -1,23 +1,23 @@
 import components
 
 def cli_coords_input() -> tuple:
-	x, y = 9, 9
-	while not (1<=x<=8 and 1<=y<=8):
+	x, y = 8, 8
+	while not (0<=x<=7 and 0<=y<=7):
 		try:
 			x = int(input("Input your x coordinate: "))
-			assert 1<=x<=8
+			assert 0<=x<=7
 			y = int(input("Input your y coordinate: "))
-			assert 1<=y<=8
+			assert 0<=y<=7
 		except (ValueError, AssertionError):
-			print("You must enter a number between 1 and 8")
+			print("You must enter a number between 0 and 7")
 	coords = (x, y)
 	return coords
 
 def move_is_available(colour: str, board: list) -> bool:
 	for i in range(8):
-		for j in range(8)
+		for j in range(8):
 			move_available = components.legal_move(colour, (i, j), board)
-			if move_available = True:
+			if move_available == True:
 				return True
 	return False	# if we complete the full loop and have not returned true,				  then we have checked every square and not found a legal move
 
@@ -45,9 +45,9 @@ def game_over(board: list):
 		print('Dark wins')
 	quit()
 
-def swap_colours(colour: str, coordinate: tuple, board: list):
+def swap_colours(colour: str, coordinate: tuple, board: list) -> list:
 	directions_to_go_in = []
-	squares_to_change = []
+	squares_to_change = [coordinate]
 	x, y = coordinate
 	if colour == 'Light':
 		opponent = 'Dark '
@@ -56,15 +56,15 @@ def swap_colours(colour: str, coordinate: tuple, board: list):
 	for i in range (-1, 2):		 # checking all the squares around the selected square
 		for j in range (-1,2):
 			try:
-				if board[(x+i)][(y+j)] == opponent:     # i want to capture this direction to check if there is
+				if board[(y+i)][(x+j)] == opponent:     # i want to capture this direction to check if there is
 						     			# a same colour counter further down
 					for k in range (1,8):
 						try:
-							if board[(x+k*i)][(y+k*j)] != opponent: # timesing k by i and j means it goes in the
+							if board[(y+k*i)][(x+k*j)] != opponent: # timesing k by i and j means it goes in the
 												# specfic direction it needs to, if the target
 												# counter is to the left (coordinate[0] - 1) then
 												# continuing to minus is what i want to do
-								if board[(x+k*i)][(y+k*j)] == 'None ':  # if we hit a blank square, exit the nested loop and check the other squares
+								if board[(y+k*i)][(x+k*j)] == 'None ':  # if we hit a blank square, exit the nested loop and check the other squares
 									break
 								else:
 									directions_to_go_in.append((i, j)) # we store the direction of the ally colour
@@ -75,12 +75,14 @@ def swap_colours(colour: str, coordinate: tuple, board: list):
 	for direction in directions_to_go_in:
 		i, j = direction
 		for squares in range (1,8):
-			if board[(x+squares*i)][(y+squares*j)] == opponent:
-				squares_to_change.append((x+squares*i), (y+squares*j))	# store the coordinates of all the squares between the ally counters
+			if board[(y+squares*i)][(x+squares*j)] == opponent:
+				squares_to_change.append((x+squares*j, y+squares*i))	# store the coordinates of all the squares between the ally counters
 			else:
 				break
-	for counters in squares_to_change:
-		board[counters[0][counters[1]] = colour		# change all the opponent counters whose location we stored to be the opposing colour
+	for squares in squares_to_change:
+		board[squares[1]][squares[0]] = colour		# change all the opponent counters whose location we stored to be the opposing colour
+	print("okay we changed it")
+	return board
 				
 
 def simple_game_loop():
@@ -88,7 +90,7 @@ def simple_game_loop():
 	board = components.initialise_board()
 	counter = 60
 	while True:
-		components.print_board()
+		components.print_board(board)
 		if counter % 2 == 0:	# if counter is even
 			colour, opponent = 'Dark ', 'Light'	# dark goes first
 		else:
@@ -98,13 +100,15 @@ def simple_game_loop():
 		if move_is_available(colour, board) == False:	# check the other colour can move
 			game_over(board)			# if neither can move, game over
 		flag = False
-		while flag = False:
+		while flag == False:
 			coords = cli_coords_input()
 			flag = components.legal_move(colour, coords, board)
-		swap_colours(colour, coords, board)
+			if flag == False:
+				print("This is not a legal move. Select different coordinates.")
+		board = swap_colours(colour, coords, board)
 		counter -= 1
 		if counter == 0:
 			game_over(board)
 
-if __name__ -- __main__:
+if __name__ == '__main__':
 	simple_game_loop()
